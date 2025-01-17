56dd318ec98afd8d15f35f5d6e5a5f264e8ddd6a
artemiy <shumk1nj@mail.ru>
2025-01-17 09:52:53 +0300

    v.0.2

--- reverence/main/models.py
+++ reverence/main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Size(models.Model):
+    name = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(unique=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['name'])]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+
+
+
+class ClothingItem(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    available = models.BooleanField(default=True)
