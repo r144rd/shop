c2d5df8ce9a6ec3cb2a18b18096a2dd3a53b3875
artemiy <shumk1nj@mail.ru>
2025-01-17 13:51:54 +0300

    -

--- reverence/main/models.py
+++ reverence/main/models.py
@@ -31,3 +31,28 @@ class ClothingItem(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
     available = models.BooleanField(default=True)
+    sizes = models.ManyToManyField(Size, through='ClothingItemSize',
+                                related_name='clothing_item', blank=True    )
+    
+    category = models.ForeignKey(Category, on_delete=models.CASCADE , related_name='clothing_items')
+    description = models.TextField(max_length=500, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add= True, null = True)
+    updated_at = models.DateTimeField(auto_now=True, )
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    discount = models.DecimalField(max_digits=5,decimal_places=2)
+    
+
+    def __str__(self):
+        return self.name
+    
+
+    def get_price_with_discount(self):
+        if self.discount > 0:
+            return self.price * (1 - (self.discount / 100))
+        return self.price
+
+
+class ClothingItemSize(models.Model):
+    clothing_item = models.ForeignKey(ClothingItem, on_delete=models)
+    
